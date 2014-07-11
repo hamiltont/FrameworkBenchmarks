d3f2d21615373a3007c49195512d4cf76eacda2e
Hamilton Turner <hamiltont@gmail.com>
2014-07-10 22:41:08 -0400

    Parse RAW results from container to load results[] in main process

--- toolset/benchmark/benchmarker.py
+++ toolset/benchmark/benchmarker.py
@@ -520,6 +520,16 @@ class Benchmarker:
       pbar.finish()
     if os.path.isfile('current_benchmark.txt'):
       os.remove('current_benchmark.txt')
+
+
+    # The test results are stored on disk, we nee to load them back 
+    # into our variables
+    # TODO - this only pulls in the result of parsing the RAW data, not 
+    # any other information that's contained within the subprocess' 
+    # results variable. Is there a better way? 
+    if self.docker:
+      test.parse_all()
+
     logging.debug("End __run_tests.")
 
     if error_happened:
