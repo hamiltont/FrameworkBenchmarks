f5aba53790bfed088c57dc5c8a76a8120d4a8027
Hamilton Turner <hamiltont@gmail.com>
2014-08-12 09:24:43 -0400

    Reduce verbosity, install prereqs, fix typos

--- toolset/setup/travis-ci/create_cache.sh
+++ toolset/setup/travis-ci/create_cache.sh
@@ -11,19 +11,26 @@ source toolset/setup/linux/bash_functions.sh
 openssl aes-256-cbc -K $encrypted_2062b438454b_key -iv $encrypted_2062b438454b_iv -in .dropbox_uploader.enc -out ~/.dropbox_uploader -d
 ls -la ~
 
-# Run the installation
+# cd into IROOT
 sudo mkdir -p $IROOT
 sudo chown -R $USER:$USER /tfb-cache
 cd $IROOT
+
+# Install prereq then our target
+. $FWROOT/toolset/setup/linux/prerequisites.sh
 fw_depends $1
+
+# Don't make the installs directory indicate this is 
+# installed, because it won't be for other workers ;-)
+rm -f fwbm_prereqs_installed
 
 # Name the tar dependency-commit.tar.gz e.g. perl-cad7a6e919a3285c6e1d6789929b1ed1e7c66fa2.tar.gz
 name=$1-${TRAVIS_COMMIT}.tar.gz
-echo using $name
 
 # Create the resulting tar file
+echo Creating $name
 cd /tfb-cache
-tar cvf $name $IROOT
+tar cf $name $IROOT
 
 # Upload tar file
 curl "https://raw.githubusercontent.com/andreafabrizi/Dropbox-Uploader/master/dropbox_uploader.sh" -o dropbox_uploader.sh
