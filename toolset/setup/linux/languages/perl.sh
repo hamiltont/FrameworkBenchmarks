b8f8cc20f3fe4dbf0a38122f4bcfd43146915462
Hamilton Turner <hamiltont@gmail.com>
2014-08-12 01:47:07 -0400

    Don't avoid building perl inside travis ;-)

--- toolset/setup/linux/languages/perl.sh
+++ toolset/setup/linux/languages/perl.sh
@@ -3,6 +3,21 @@
 RETCODE=$(fw_exists perl-5.18)
 [ ! "$RETCODE" == 0 ] || { return 0; }
 
+# If we are running inside travis, download a pre-built 
+# installs directory
+#if [ "$TRAVIS" = "true" ]
+#  then
+#  echo Inside Travis-CI, using pre-built Perl installation
+#  fw_get https://db.tt/XwP474Ng -O perl-5.18.tar.gz
+#  fw_untar perl-5.18.tar.gz -C $FWROOT
+#  
+#  cd perl-5.18
+#  grep -rl '/localhdd/installs' ./ | xargs sed -i "s|/localhdd/installs|$IROOT|g"
+#  echo "looking at cpanm"
+#  head bin/cpanm
+#  return 0
+#fi
+
 fw_get https://raw.github.com/tokuhirom/Perl-Build/master/perl-build -O perl-build.pl
 # compile with optimizations, n.b. this does not turn on debugging
 perl perl-build.pl -DDEBUGGING=-g 5.18.2 perl-5.18
