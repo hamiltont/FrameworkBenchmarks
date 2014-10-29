f7372ab46260bfbc2090d47a32fffcda0c0a5502
Hamilton Turner <hamiltont@gmail.com>
2014-10-29 18:50:26 -0400

    DO NOT MERGE (having openresty use multiprocessing)

--- frameworks/Lua/openresty/setup.py
+++ frameworks/Lua/openresty/setup.py
@@ -2,12 +2,19 @@ import subprocess
 import sys
 import setup_util
 import os
+import multiprocessing
 
 def start(args, logfile, errfile):
   setup_util.replace_text("openresty/nginx.conf", "CWD", args.troot)
   setup_util.replace_text("openresty/app.lua", "DBHOSTNAME", args.database_host)
-  subprocess.Popen('sudo /usr/local/openresty/nginx/sbin/nginx -c $TROOT/nginx.conf -g "worker_processes ' + str((args.max_threads)) + ';"', shell=True, cwd="openresty", stderr=errfile, stdout=logfile)
 
+  threads = multiprocessing.cpu_count()
+  logfile.write("Starting %s workers\n\n" % threads)
+  logfile.flush()
+  errfile.write("Starting %s workers\n\n" % threads)
+  errfile.flush()
+  print "Starting %s workers" %  threads
+  subprocess.Popen('sudo /usr/local/openresty/nginx/sbin/nginx -c $TROOT/nginx.conf -g "worker_processes ' + str(threads) + ';"', shell=True, cwd="openresty", stderr=errfile, stdout=logfile)
   return 0
 
 def stop(logfile, errfile):
